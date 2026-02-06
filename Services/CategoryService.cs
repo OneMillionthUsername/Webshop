5666daaacd859c0d57f373d4718cb3b474a4a190
OneMillionthUsername <dean.marinov@gmx.at>
2026-02-06 14:28:17 +0100

    Kategorie-Service-Schicht hinzugefügt und View angepasst
    Neue Service-Schicht für Kategorien mit Interface und Implementierung erstellt (aktuell NotImplemented). Im Index.cshtml wurde ein foreach-Block ergänzt, um Kategorien aus dem Model in einer Tabelle darzustellen.

---
+++ Services/CategoryService.cs
@@ -0,0 +1,39 @@
+﻿using Webshop.Data;
+using Webshop.Models;
+
+namespace Webshop.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+		}
+
+		public Task CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetCategoryByIdAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Webshop.Models;
+
+namespace Webshop.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(int categoryId);
+        Task CreateCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(int categoryId);
+	}
+}

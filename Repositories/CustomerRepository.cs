fc7260a7b205b09c8b9dc15c781f98dd450bb66e
OneMillionthUsername <dean.marinov@gmx.at>
2026-02-07 22:48:10 +0100

    CustomerRepository und Interface hinzugefügt

---
+++ Repositories/CustomerRepository.cs
@@ -0,0 +1,47 @@
+﻿using Webshop.Models;
+
+namespace Webshop.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public Task<Customer> AddAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCustomerByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer>? GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer>? GetByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer>? GetByOrderIdAsync(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> UpdateAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repositories/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Webshop.Models;
+
+namespace Webshop.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task<Customer>? GetByIdAsync(int id);
+        Task<Customer>? GetByNameAsync(string name);
+        Task<Customer>? GetByOrderIdAsync(int orderId);
+        Task<Customer> AddAsync(Customer customer);
+        Task<Customer> UpdateAsync(Customer customer);
+        Task DeleteCustomerByIdAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
